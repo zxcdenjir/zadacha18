4b4fa64a54823941c24ffcd0d586bf41c45b941a
zxcdenjir <rush.wars1592@gmail.com>
2025-04-26 14:50:14 +0300

    Добавьте файлы проекта.

---
+++ Auto.cs
@@ -0,0 +1,127 @@
+﻿class Auto
+{
+    private Random random = new();
+
+    private string? Number;
+    private float Fuel;
+    private float Rashod;
+    private int Probeg;
+    private bool isMoving = false;
+    private bool isAvaria = false;
+
+    public void Info(string nom, float bak, float ras)
+    {
+        Number = nom;
+        Fuel = bak;
+        Rashod = ras;
+        Probeg = 0;
+    }
+    public void Out()
+    {
+        Console.Write("Номер автомобиля: " + Number);
+        Console.Write(" | Остаток топлива: " + $"{Ostatok():F2}" + " л");
+        Console.WriteLine(" | Расход топлива на 100 км: " + Rashod + " л");
+    }
+    public void Zapravka(float top)
+    {
+        Console.WriteLine();
+        Fuel += top;
+        Console.WriteLine($"Автомобиль заправлен на {top} литров\nТекущее количества топлива: {Ostatok():F2} л ");
+    }
+    public void Move(int km)
+    {
+        Console.WriteLine();
+        if (Calculate(km))
+        {
+            double avariaChance = random.NextDouble();
+            if (avariaChance <= 0.10)
+            {
+                int avariaKm = random.Next(1, km);
+                float requiredFuel = Rashod / 100 * avariaKm;
+                Fuel -= requiredFuel;
+                Probeg += avariaKm;
+                Avariya(avariaKm);
+                return;
+            }
+            else
+            {
+                float requiredFuel = Rashod / 100 * km;
+                Fuel -= requiredFuel;
+                Probeg += km;
+                Console.WriteLine($"Машина проехала {km} км. Остаток топлива: {Fuel:F2} л\n");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Недостаточно топлива для поездки.");
+            Tormoz();
+            Console.Write("Хотите заправить автомобиль? (да/нет): ");
+            string answer = Console.ReadLine();
+            Console.WriteLine();
+            if (answer.ToLower() == "да" || answer == "")
+            {
+                Console.WriteLine("Сколько литров хотите заправить? ");
+                do
+                {
+                    float fuelForZapravka = Program.FloatInput("Введите количество топлива для заправки: ", 0);
+                    Zapravka(fuelForZapravka);
+                    if (!Calculate(km))
+                        Console.WriteLine("Всё ещё мало топлива, нужно дозаправить.");
+                } while (!Calculate(km));
+                Razgon();
+                Move(km);
+            }
+        }
+    }
+    private float Ostatok()
+    {
+        return Fuel;
+    }
+    public bool Calculate(int distance)
+    {
+        float tmp_fuel = Fuel;
+        while (distance > 0)
+        {
+            float requiredFuel = Rashod / 100;
+            if (tmp_fuel >= requiredFuel)
+            {
+                tmp_fuel = float.Round(tmp_fuel - requiredFuel, 2);
+                distance--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void Razgon()
+    {
+        if (!isMoving)
+        {
+            isMoving = true;
+            Console.WriteLine();
+            Console.WriteLine("Автомобиль разгоняется...");
+        }
+    }
+    public void Tormoz()
+    {
+        if (isMoving)
+        {
+            isMoving = true;
+            Console.WriteLine("Автомобиль тормозит...");
+            Console.WriteLine();
+        }
+    }
+    public int GetProbeg()
+    {
+        return Probeg;
+    }
+    public void Avariya(int km)
+    {
+        Console.WriteLine($"Автомобиль попал в аварию на {km} км и не может продолжать движение.");
+        isAvaria = true;
+    }
+    public bool AvariyaStatus() { return isAvaria; }
+
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+
+        Auto[] autos = [
+            new Auto(),
+            new Auto(),
+            new Auto()
+        ];
+        autos[0].Info("A123BC", 0, 5);
+        autos[1].Info("B456CD", 0, 7);
+        autos[2].Info("C789EF", 0, 10);
+
+        PrintAllCars(autos);
+
+        Auto currentCar = autos[IntInput("Выберите автомобиль: ", 1, autos.Length) - 1];
+
+        Console.WriteLine();
+        currentCar.Out();
+
+        float fuelAmount = FloatInput("Введите количество топлива для заправки: ", 0);
+        currentCar.Zapravka(fuelAmount);
+        
+        Drive(currentCar);
+
+    }
+
+    static void PrintAllCars(Auto[] autos)
+    {
+        for (int i = 0; i < autos.Length; i++)
+        {
+            Console.Write($"Автомобиль {i + 1}: ");
+            autos[i].Out();
+        }
+        Console.WriteLine();
+    }
+
+    static void Drive(Auto car)
+    {
+        Random random = new();
+
+        bool continueDriving = true;
+        int distance = random.Next(5, 100);
+        if (car.Calculate(distance))
+            car.Razgon();
+        do
+        {
+            Console.WriteLine($"Случайная дистанция: {distance} км");
+            car.Move(distance);
+
+            if (car.AvariyaStatus())
+                break;
+
+            Console.Write("Продолжить движение? (да/нет): ");
+            string answer = Console.ReadLine();
+            if (!(answer.ToLower() == "да" || answer == ""))
+                continueDriving = false;
+            distance = random.Next(5, 100);
+            Console.WriteLine();
+        } while (continueDriving);
+        car.Tormoz();
+        Console.WriteLine($"Было пройдено {car.GetProbeg()} км");
+    }
+    
+    public static float FloatInput(string text, float minValue)
+    {
+        float value;
+        bool isCorrect;
+        do
+        {
+            Console.Write(text);
+            isCorrect = float.TryParse(Console.ReadLine(), out value);
+            if (!isCorrect)
+                Console.WriteLine("Неверный ввод данных");
+            else if (value <= minValue)
+                Console.WriteLine($"Значение должно быть больше {minValue}");
+        } while (!isCorrect || value <= minValue);
+        return value;
+    }
+
+    static int IntInput(string text, int minValue, int maxValue)
+    {
+        int value;
+        bool isCorrect;
+        do
+        {
+            Console.Write(text);
+            isCorrect = int.TryParse(Console.ReadLine(), out value);
+            if (!isCorrect)
+                Console.WriteLine("Неверный ввод данных");
+            else if (value < minValue || value > maxValue)
+                Console.WriteLine($"Некорректный ввод. Пожалуйста, введите число от {minValue} до {maxValue}");
+        } while (!isCorrect || value < minValue || value > maxValue);
+        return value;
+    }
+}
